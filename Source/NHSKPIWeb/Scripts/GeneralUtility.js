910ce706150bc9dfe2d1289afc7302ce6951b503
brianperera <briano_p@yahoo.com>
2014-10-18 02:51:12 -0700

    Check-in code for requirement 1 and 2

---
+++ Source/NHSKPIWeb/Scripts/GeneralUtility.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    ManageManualHospitalEntryField();
+    
+});
+
+function ManageManualHospitalEntryField() {
+    var hospitalDDLSelection = $("#ddlHospitalName").value;
+
+    if (hospitalDDLSelection == "More") {
+        alert("More");
+        $("#txtCompanyName").show();
+    }
+}
