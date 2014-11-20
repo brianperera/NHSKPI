eb5c4b0f65ac02fa7e7906dc3e57f48a48c3b4a1
brianperera <briano_p@yahoo.com>
2014-11-20 22:00:35 +0530

    Adding the data export page

---
+++ Source/NHSKPIWeb/Views/KPI/DataExport.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="NHS KPI Data Entry Portal" Language="C#" MasterPageFile="~/Views/Shared/MasterPage.master"
+    AutoEventWireup="true" CodeFile="DataExport.aspx.cs" Inherits="Views_KPI_DataExport" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Contentbody" runat="Server">
+   
+    <asp:ListBox ID="ListBox1" Width="200px" runat="server"></asp:ListBox>
+   
+&nbsp;
+    <asp:Button ID="Button1" runat="server" Text=">" />
+    &nbsp;<asp:Button ID="Button2" runat="server" Text="<" />
+&nbsp;<asp:ListBox ID="ListBox2" Width="200px" runat="server"></asp:ListBox>
+   
+    <br />
+    <asp:Button ID="Button3" runat="server" Text="Export Ward Data" />
+&nbsp;<asp:Button ID="Button4" runat="server" Text="Export Specialty Data" />
+   
+</asp:Content>
---
+++ Source/NHSKPIWeb/Views/KPI/DataExport.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NHSKPIBusinessControllers;
+using NHSKPIDataService;
+using NHSKPIDataService.Util;
+using NHSKPIDataService.Models;
+
+public partial class Views_KPI_DataExport : System.Web.UI.Page
+{
+    #region Private Variables
+
+    private KPIController kpiController = null;
+    private NHSKPIDataService.Models.KPI kpi = null;
+    private int kpiId;
+    private User nhsUser = null;
+
+    #endregion
+
+    #region Public Properties
+
+    public KPIController KPIController
+    {
+        get
+        {
+            if (kpiController == null)
+            {
+                kpiController = new KPIController();
+            }
+
+            return kpiController;
+        }
+        set
+        {
+            kpiController = value;
+        }
+    }
+    public NHSKPIDataService.Models.KPI Kpi
+    {
+        get 
+        {
+            if (kpi == null)
+            {
+                kpi = new NHSKPIDataService.Models.KPI();
+            }
+            return kpi; 
+        }
+        set 
+        { 
+            kpi = value; 
+        }
+    }
+    public int KpiId
+    {
+        get 
+        {
+            if (Request.QueryString["Id"] != null && int.Parse(Request.QueryString["Id"].ToString()) > 0)
+            {
+                kpiId = int.Parse(Request.QueryString["Id"].ToString());
+            }
+            else
+            {
+                kpiId = 0;
+            }
+            return kpiId; 
+        }
+        set 
+        { 
+            kpiId = value; 
+        }
+    }
+    public User NHSUser
+    {
+        get
+        {
+            if (Session["NHSUser"] != null)
+            {
+                nhsUser = (User)Session["NHSUser"];
+            }
+            else
+            {
+                Response.Redirect("~/login.aspx");
+            }
+
+            return nhsUser;
+        }
+        set
+        {
+            nhsUser = value;
+        }
+    }
+
+ 
+
+    #endregion
+
+    #region Page Load
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            
+        }
+    }
+    #endregion   
+   
+}
