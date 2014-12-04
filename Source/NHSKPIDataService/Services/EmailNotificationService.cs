438fb5a3d7c13976dc4688c1f22af30360a3b0e4
brianperera <briano_p@yahoo.com>
2014-12-05 00:46:29 +0530

    Email notification related changes

---
+++ Source/NHSKPIDataService/Services/EmailNotificationService.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using NHSKPIDataService.Models;
+using NHSKPIDataService.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+
+namespace NHSKPIDataService.Services
+{
+    public class EmailNotificationService
+    {
+        #region private varibles
+
+        private DbTransaction transaction;
+        private DbConnection connection;
+
+        #endregion  
+
+        #region Methods
+        public void InsertEmailNotification(EmailNotification emailNotification)
+        {
+            try
+            {
+                Database db = DatabaseFactory.CreateDatabase(Constant.NHS_Database_Connection_Name);
+                connection = db.CreateConnection();
+                connection.Open();
+                transaction = connection.BeginTransaction();
+
+                emailNotification.Add(db, transaction);
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+        }
+
+        public void UpdateEmailNotification(EmailNotification emailNotification)
+        {
+            try
+            {
+                Database db = DatabaseFactory.CreateDatabase(Constant.NHS_Database_Connection_Name);
+                connection = db.CreateConnection();
+                connection.Open();
+                transaction = connection.BeginTransaction();
+
+                emailNotification.Update(db, transaction);
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+        }
+
+        public EmailNotification SearchEmailNotification(EmailNotification emailNotification, int hospitalId)
+        {
+            try
+            {
+                Database db = DatabaseFactory.CreateDatabase(Constant.NHS_Database_Connection_Name);
+                connection = db.CreateConnection();
+                connection.Open();
+                transaction = connection.BeginTransaction();
+
+                var result = emailNotification.Search(db, transaction, hospitalId);
+
+                transaction.Commit();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+        }
+
+        public List<EmailNotification> SearchAllEmailNotifications(EmailNotification emailNotification)
+        {
+            try
+            {
+                Database db = DatabaseFactory.CreateDatabase(Constant.NHS_Database_Connection_Name);
+                connection = db.CreateConnection();
+                connection.Open();
+                transaction = connection.BeginTransaction();
+
+                var result = emailNotification.SearchAll(db, transaction);
+
+                transaction.Commit();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+        }
+
+        public void DeleteEmailNotification(EmailNotification emailNotification)
+        {
+            try
+            {
+                Database db = DatabaseFactory.CreateDatabase(Constant.NHS_Database_Connection_Name);
+                connection = db.CreateConnection();
+                connection.Open();
+                transaction = connection.BeginTransaction();
+
+                emailNotification.Delete(db, transaction);
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+        }
+        #endregion
+    }
+}
