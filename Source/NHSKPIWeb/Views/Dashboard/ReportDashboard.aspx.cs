d2a160d85aa423dcb68b523e5ea2b3588ddd59c4
brianperera <briano_p@yahoo.com>
2014-12-11 00:48:51 +0530

    Adding the report dash board page

--- Source/NHSKPIWeb/Views/Dashboard/ReportDashboard.aspx.cs
+++ Source/NHSKPIWeb/Views/Dashboard/ReportDashboard.aspx.cs
@@ -23,24 +23,24 @@ public partial class Views_Dashboard_ReportDashboard : System.Web.UI.Page
 
     protected void btnSave_Click(object sender, EventArgs e)
     {
-        Response.ContentType = "application/pdf";
-        Response.AddHeader("content-disposition",
-         "attachment;filename=GridViewExport.pdf");
-        Response.Cache.SetCacheability(HttpCacheability.NoCache);
-        StringWriter sw = new StringWriter();
-        HtmlTextWriter hw = new HtmlTextWriter(sw);
-        GridView1.AllowPaging = false;
-        GridView1.DataBind();
-        GridView1.RenderControl(hw);
-        StringReader sr = new StringReader(sw.ToString());
-        Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
-        HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
-        PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
-        pdfDoc.Open();
-        htmlparser.Parse(sr);
-        pdfDoc.Close();
-        Response.Write(pdfDoc);
-        Response.End();
+        //Response.ContentType = "application/pdf";
+        //Response.AddHeader("content-disposition",
+        // "attachment;filename=GridViewExport.pdf");
+        //Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        //StringWriter sw = new StringWriter();
+        //HtmlTextWriter hw = new HtmlTextWriter(sw);
+        //GridView1.AllowPaging = false;
+        //GridView1.DataBind();
+        //GridView1.RenderControl(hw);
+        //StringReader sr = new StringReader(sw.ToString());
+        //Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+        //HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+        //PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+        //pdfDoc.Open();
+        //htmlparser.Parse(sr);
+        //pdfDoc.Close();
+        //Response.Write(pdfDoc);
+        //Response.End();
     }
 
     #endregion
